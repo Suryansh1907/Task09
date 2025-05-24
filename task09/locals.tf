2d64b52e6602aac2234bafe56d50595af8d139c4
Suryansh Tilak <suryansh_tilak@epam.com>
2025-05-24 14:34:00 +0530

    c4

--- task09/locals.tf
+++ task09/locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_prefix      = "cmtr-eh8dj90z-mod9"
   vnet_address_space   = "10.0.0.0/16"
-  firewall_subnet_cidr = cidrsubnet(local.vnet_address_space映, 8, 1) # Generates 10.0.1.0/24
+  firewall_subnet_cidr = cidrsubnet(local.vnet_address_space, 8, 1) # Generates 10.0.1.0/24
 }
